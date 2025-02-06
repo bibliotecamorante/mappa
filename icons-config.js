e4fc7db01f93cc0a37078fb343d7db8dfc0b7b6d
bibliotecamorante <bibliotecamorante@gmail.com>
2025-02-06 09:29:02 +0100

    Update icons-config.js

--- icons-config.js
+++ icons-config.js
@@ -119,7 +119,75 @@ municipio1: L.icon({
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
         shadowSize: [41, 41]
-    })
+    }),
+
+        // Aggiungi le nuove configurazioni per le biblioteche federate
+        uniroma3: L.icon({
+            iconUrl: 'unirosso.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [50, 50], // Aumentata larghezza e altezza
+    iconAnchor: [25, 50], // Centrato il punto di ancoraggio
+    popupAnchor: [1, -40], // Leggermente adattato
+    shadowSize: [50, 50] // Adattata l'ombra
+        }),
+
+    lumsa: L.icon({
+            iconUrl: 'univerde.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [50, 50], // Aumentata larghezza e altezza
+    iconAnchor: [25, 50], // Centrato il punto di ancoraggio
+    popupAnchor: [1, -40], // Leggermente adattato
+    shadowSize: [50, 50] // Adattata l'ombra
+        }),
+
+    foro: L.icon({
+            iconUrl: 'univiola.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [50, 50], // Aumentata larghezza e altezza
+    iconAnchor: [25, 50], // Centrato il punto di ancoraggio
+    popupAnchor: [1, -40], // Leggermente adattato
+    shadowSize: [50, 50] // Adattata l'ombra
+        }),
+
+      link: L.icon({
+    iconUrl: 'unigrigio.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [50, 50], // Aumentata larghezza e altezza
+    iconAnchor: [25, 50], // Centrato il punto di ancoraggio
+    popupAnchor: [1, -40], // Leggermente adattato
+    shadowSize: [50, 50] // Adattata l'ombra
+        }),
+    
+    
+        castelli: L.icon({
+    iconUrl: 'comuneblu.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [50, 50], // Aumentata larghezza e altezza
+    iconAnchor: [25, 50], // Centrato il punto di ancoraggio
+    popupAnchor: [1, -40], // Leggermente adattato
+    shadowSize: [50, 50] // Adattata l'ombra
+}),
+
+        ceretano: L.icon({
+    iconUrl: 'comunemagenta.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [50, 50], // Aumentata larghezza e altezza
+    iconAnchor: [25, 50], // Centrato il punto di ancoraggio
+    popupAnchor: [1, -40], // Leggermente adattato
+    shadowSize: [50, 50] // Adattata l'ombra
+}),
+    
+        prenestini: L.icon({
+    iconUrl: 'comunemarrone.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [50, 50], // Aumentata larghezza e altezza
+    iconAnchor: [25, 50], // Centrato il punto di ancoraggio
+    popupAnchor: [1, -40], // Leggermente adattato
+    shadowSize: [50, 50] // Adattata l'ombra
+})
+
+
+    
        
   };
 }
